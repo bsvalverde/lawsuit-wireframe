e29d48de4cd3816c2481ac7d02b63d5c7f7a9c8a
Bernardo <bsvalverde@gmail.com>
2019-08-23 11:13:05 -0300

    Case history available

--- src/components/Cases/HistoryModal/HistoryModal.js
+++ src/components/Cases/HistoryModal/HistoryModal.js
@@ -2,18 +2,35 @@ import React from 'react';
 
 import classes from './HistoryModal.module.scss';
 
+import cloud from '../../../assets/icons/cloud.svg';
+
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const HistoryModal = (props) => {
   const legalCase = props.case;
 
-  let history = <p>Nada para mostrarto.</p>
+  let history = <p className={classes.Empty}>Nada para mostrar.</p>
   if (legalCase.historicals.length > 0) {
-    history = legalCase.historicals.map((legalCase, index) => {
-      return <p key={index}>histórico</p>;
-    });
+    history = (
+      <table>
+        <tbody>
+          {
+            legalCase.historicals.map((historical, index) => {
+              const [date,] = historical.date.split('T');
+              return (
+                <tr key={index}>
+                  <td><img src={cloud} alt="Cloud" /></td>
+                  <td>{date.replace(/-/g, '/')}</td>
+                  <td><p>{historical.description}</p></td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    );
   }
-console.log(legalCase);
+
   return (
     <Backdrop>
       <div className={classes.Modal}>
@@ -23,7 +40,8 @@ console.log(legalCase);
           onClick={props.close}
         ></button>
       <div className={classes.Box}>
-        {history}
+        <p className={classes.Title}>Históricos</p>
+        { history }
       </div>
       </div>
     </Backdrop>
